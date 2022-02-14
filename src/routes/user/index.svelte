f7d5476c643fe98406811877816bb4200d95ad86
Domenik Reitzner <d.reitzner@emakina.at>
2022-02-14 17:02:03 +0100

    Add user route

---
+++ src/routes/user/index.svelte
@@ -0,0 +1,22 @@
+<script context="module" lang="ts">
+    export async function load({fetch}) {
+        const response = await fetch('/.auth/me');
+
+		return {
+			status: response.status,
+			props: {
+				json: response.ok && (await response.json())
+			}
+		};
+    }
+</script>
+
+<script lang="ts">
+    export let json: any;
+</script>
+<h1>Auth Me response</h1>
+<pre>
+    <code>
+        {JSON.stringify(json, null, 2)}
+    </code>
+</pre>
